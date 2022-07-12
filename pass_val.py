384da9a798189ff0424fe5bd3a6d4abccf082884
KillerHealer <noam142196@gmail.com>
2022-07-12 20:13:41 +0300

    First commit!

---
+++ pass_val.py
@@ -0,0 +1,6 @@
+def pass_val(pw: str):
+        """
+        A program that validates a password's strength
+        :param pw: The password to be validated
+        :return: exit code 0/1
+        """
