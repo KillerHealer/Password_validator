749068110eb261c1b8601d041a54072c1ed34b75
KillerHealer <noam142196@gmail.com>
2022-07-12 22:30:20 +0300

    home work number 1

--- password_validator.py
+++ password_validator.py
@@ -1,43 +1,13 @@
-import sys
-
-from colorama import Fore
-
-
-def pass_val(pw: str):
-    """
-        A program that validates a password's strength
-        :param pw: The password to be validated
-        :return: exit code 0/1
-        """
-    lo, upp, num, length = False, False, False, False
-    if len(pw) >= 10:
-        length = True
-        for char in pw:
-            if char.islower():
-                lo = True
-            if char.isupper():
-                upp = True
-            if char.isnumeric():
-                num = True
-    if lo and upp and num and length:
-        print(Fore.GREEN + "This password is Valid!")
-    else:
-        if not length:
-            print(Fore.RED + "This password is invalid because there are not enough characters!")
-            exit(1)
-        if not lo:
-            print(Fore.RED + "This password is invalid because there are no lowercase letters!")
-            exit(1)
-        if not upp:
-            print(Fore.RED + "This password is invalid because there are no uppercase letters!")
-            exit(1)
-        if not num:
-            print(Fore.RED + "This password is invalid because there are no numbers!")
-            exit(1)
+# 1
+lst = [3, 7, 2]
+histo = []
+for num in lst:
+    histo.append("*" * num)
+print (histo)
 
 
-if sys.argv[1] != '-f':
-    pass_val(str(sys.argv[1]))
-else:
-    f = open(sys.argv[2])
-    pass_val(f.read())
+# 2
+s1 = "asdfgh"
+s2 = "hgfdsa"
+s3 = s1 + s2[::-1]
+print(s3)
