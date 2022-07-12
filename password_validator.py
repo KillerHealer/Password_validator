9d8b57f83435c8b09d07dbd6a00d1a52e0f1ed64
KillerHealer <noam142196@gmail.com>
2022-07-12 21:36:05 +0300

    test complete

---
+++ password_validator.py
@@ -0,0 +1,37 @@
+from colorama import Fore
+
+
+def pass_val(pw: str):
+    """
+        A program that validates a password's strength
+        :param pw: The password to be validated
+        :return: exit code 0/1
+        """
+    lo, upp, num, length = False, False, False, False
+    if len(pw) >= 10:
+        length = True
+        for char in pw:
+            if char.islower():
+                lo = True
+            if char.isupper():
+                upp = True
+            if char.isnumeric():
+                num = True
+    if lo and upp and num and length:
+        print(Fore.GREEN + "This password is Valid!")
+    else:
+        if not length:
+            print(Fore.RED + "This password is invalid because there are not enough characters!")
+            exit(1)
+        if not lo:
+            print(Fore.RED + "This password is invalid because there are no lowercase letters!")
+            exit(1)
+        if not upp:
+            print(Fore.RED + "This password is invalid because there are no uppercase letters!")
+            exit(1)
+        if not num:
+            print(Fore.RED + "This password is invalid because there are no numbers!")
+            exit(1)
+
+
+pass_val("kjlijaaA1")
