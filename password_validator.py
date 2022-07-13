f515fdc23140f683fa608ae95db36dbac4dbbb8f
KillerHealer <noam142196@gmail.com>
2022-07-13 22:32:47 +0300

    home work number 3

--- password_validator.py
+++ password_validator.py
@@ -1,46 +1,48 @@
-from collections import defaultdict
-# A1
-s = "mvnlskjhlasdkjnfmlkjsaf"
-d = {}
-for char in s:
-    d[char] = d.get(char, 0) + 1
+from statistics import median
 
 
-print(sorted(d.items()))
-# A2
-reverseDict = defaultdict(list)
-{reverseDict[v].append(k) for k, v in d.items()}
-result = dict(reverseDict)
-print(result)
-# B1
-l1 = [1, 2, 3, 3, 4, 5, 6, 7]
-l2 = [2, 4, 12, 31, 21]
-l3 = [54, 34, 23, 4, 2]
-all_lst_dict = {"l1": l1, "l2": l2, "l3": l3}
-fit_dic = {}
-for name, lst in all_lst_dict.items():
-    tmp_set = set(lst)
-    if len(tmp_set) != len(lst):
-        fit_dic[name] = tmp_set
-        print(f"the list {lst} has at least one repeating items")
-print()
-# B2
-common_values = set.intersection(*fit_dic.values())
-print("B:")
-print(common_values, " ".join(fit_dic.keys()))
-print()
-# C1
-lastl = [31, 99, 3, 1943]
-x1 = []
-for item in lastl:
-    x1.append([int(a) for a in str(item)])
-res1 = []
-for item in x1:
-    for item2 in item:
-        res1.append(item2)
-res1 = set(res1)
-sort_order = "desc"
-if sort_order == "desc":
-    print(sorted(res1, reverse=True))
-if sort_order == "asc":
-    print(sorted(res1))
+def split_male_female(args):
+    female_set = {}
+    male_set = {}
+    for key, item in args.items():
+        if item["sex"] == "male":
+            male_set[key] = item
+        if item["sex"] == "female":
+            female_set[key] = item
+    return female_set, male_set
+
+
+def find_median_average(args):
+    age_sum = 0
+    age_median = {}
+    for key, item in args.items():
+        age_sum += item["age"]
+        age_median[key] = item["age"]
+    age_avg = age_sum / len(args)
+    age_med_num = median(age_median.values())
+    print(f"The average age of this group of people is {age_avg}")
+    print(f"The median age of this group of people is {age_med_num}")
+
+
+def print_values_above(args, num=0):
+    if num <= 0:
+        print(args)
+    for key, item in args.items():
+        if item["age"] > num:
+            print(item)
+
+
+def main():
+    data_set = {
+        1: {"name": "Noam", "age": 26, "sex": "male"},
+        2: {"name": "Tal", "age": 25, "sex": "female"},
+        3: {"name": "Omri", "age": 37, "sex": "male"}
+    }
+    female_set, male_set = split_male_female(data_set)
+    print(f"These are the males: {male_set}")
+    print(f"These are the females: {female_set}")
+    find_median_average(data_set)
+    print_values_above(data_set)
+
+
+main()
